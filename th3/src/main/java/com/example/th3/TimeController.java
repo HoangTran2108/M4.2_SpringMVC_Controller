62e577e3890feb845da70e7f093495dd9453bee9
HoangTran2108 <119851922+HoangTran2108@users.noreply.github.com>
2023-03-23 15:37:07 +0700

    [Thực hành] Ứng dụng quản lý danh sách khách hàng

--- th3/src/main/java/com/example/th3/TimeController.java
+++ th3/src/main/java/com/example/th3/TimeController.java
@@ -11,7 +11,7 @@ import java.util.TimeZone;
 @Controller
 public class TimeController {
 
-    @GetMapping("/")
+    @GetMapping("/world-clock")
     public String getTimeByTimezone(ModelMap model, @RequestParam(name = "city", required = false, defaultValue = "Asia/Ho_Chi_Minh") String city) {
         // Lấy ra thời gian hiện tại
         Date date = new Date();
